71d0ec838df9591338e9f5bd6cd357081e0174ac
Fabio Squizzato <squizzato.fabio@gmail.com>
2025-12-22 15:23:04 -0300

    lesson 2: controllers skeleton

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+    @Get() // GET /users or users?role=admin
+    findAll(@Query('role') role?: 'INTERN' | 'ADMIN', @Query('active') active?: boolean, @Query('age') age?: number) {
+        return role ? `All users with role: ${role}, active: ${active}, age: ${age}` : 'All users';
+    }
+
+    @Get('interns') // GET /users/interns => order matters
+    findInterns() {
+        return [];
+    }
+
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    @Post() // POST /users
+    create(@Body() user: {}) {
+        return { user };
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    findOneAndUpdate(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { ...userUpdate, id };
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    findOneAndDelete(@Param('id') id: string) {
+        return { id };
+    }
+
+
+}
