a29d5ba6b0c1ff2beeac8b08e3e6a2f740b1d49d
Fabio Squizzato <squizzato.fabio@gmail.com>
2025-12-23 17:34:05 -0300

    feat: users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
+
+    // Dependency Injection
+    constructor(private readonly usersService: UsersService) {}
+
     /*
     GET /users
     GET /users/:id
@@ -10,8 +14,8 @@ export class UsersController {
     DELETE /users/:id
     */
     @Get() // GET /users or users?role=admin
-    findAll(@Query('role') role?: 'INTERN' | 'ADMIN', @Query('active') active?: boolean, @Query('age') age?: number) {
-        return role ? `All users with role: ${role}, active: ${active}, age: ${age}` : 'All users';
+    findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER', @Query('active') active?: boolean, @Query('age') age?: number) {
+        return this.usersService.findAll(role);
     }
 
     @Get('interns') // GET /users/interns => order matters
@@ -21,22 +25,22 @@ export class UsersController {
 
     @Get(':id') // GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id };
+        return this.usersService.findOne(+id);
     }
 
     @Post() // POST /users
-    create(@Body() user: {}) {
-        return { user };
+    create(@Body() user: {name: string; role: 'INTERN' | 'ADMIN' | 'ENGINEER'; email: string; username?: string; phone?: string; website?: string;}) {
+        return this.usersService.create(user);
     }
 
     @Patch(':id') // PATCH /users/:id
     findOneAndUpdate(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { ...userUpdate, id };
+        return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(':id') // DELETE /users/:id
     findOneAndDelete(@Param('id') id: string) {
-        return { id };
+        return this.usersService.delete(+id);
     }
 
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,128 @@
 import { Injectable } from '@nestjs/common';
+import { create } from 'domain';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      role: 'ENGINEER',
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+    },
+    {
+      id: 2,
+      role: 'ADMIN',
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+    },
+    {
+      id: 3,
+      role: 'INTERN',
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+    },
+    {
+      id: 4,
+      role: 'ENGINEER',
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+    },
+    {
+      id: 5,
+      role: 'ADMIN',
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+    },
+    {
+      id: 6,
+      role: 'INTERN',
+      name: 'Mrs. Dennis Schulist',
+      email: 'Karley_Dach@jasper.info',
+    },
+    {
+      id: 7,
+      role: 'ENGINEER',
+      name: 'Kurtis Weissnat',
+      email: 'Telly.Hoeger@billy.biz',
+    },
+    {
+      id: 8,
+      role: 'ADMIN',
+      name: 'Nicholas Runolfsdottir V',
+      email: 'Sherwood@rosamond.me',
+    },
+    {
+      id: 9,
+      role: 'INTERN',
+      name: 'Glenna Reichert',
+      email: 'Chaim_McDermott@dana.io',
+    },
+    {
+      id: 10,
+      role: 'ENGINEER',
+      name: 'Clementina DuBuque',
+      email: 'Rey.Padberg@karina.biz',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ADMIN' | 'ENGINEER'): any[] {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number): any {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    role: 'INTERN' | 'ADMIN' | 'ENGINEER';
+    email: string;
+    username?: string;
+    phone?: string;
+    website?: string;
+  }) {
+    const highestId = this.users
+      ? this.users.reduce((max, user) => (user.id > max ? user.id : max), 0)
+      : 0;
+    const newUser = {
+      id: highestId + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    userUpdate: Partial<{
+      name: string;
+      role: 'INTERN' | 'ADMIN' | 'ENGINEER';
+      email: string;
+    }>,
+  ) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return null;
+    }
+    const updatedUser = {
+      ...this.users[userIndex],
+      ...userUpdate,
+    };
+    this.users[userIndex] = updatedUser;
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    if (removedUser === -1) {
+      return null;
+    }
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
